928b7c26c7e3be51e278f01c726028c9c3e50e70
Yusho Yamaguchi <yusho.yamaguchi@sony.com>
2024-06-11 18:19:11 +0900

    built libbpf (has bug)

---
+++ ebpf_9f/event1.bpf.c
@@ -0,0 +1,28 @@
+#include "include/vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+struct data_t {
+    __s32 pkt_len;
+};
+
+// Define the perf event array map
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(max_entries, 128);
+} xdp_perf_event_map SEC(".maps");
+
+SEC("xdp")
+int xdp_prog(struct xdp_md *ctx) {
+    struct data_t data = {};
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data_start = (void *)(long)ctx->data;
+
+    data.pkt_len = data_end - data_start;
+
+    // Output the event
+    bpf_perf_event_output(ctx, &xdp_perf_event_map, BPF_F_CURRENT_CPU, &data, sizeof(data));
+
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
