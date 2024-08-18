3320172e04b59e5a4b062acf33ad827bf4437b0a
yushoyamaguchi <ysh.824@outlook.jp>
2024-08-19 00:08:49 +0900

    kretprobe of handle_io

---
+++ ebpf_flab/kretprobe_kvm/port_trace.bpf.c
@@ -0,0 +1,44 @@
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+
+#define TARGET_PORT 0x84
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, __u32);
+    __type(value, __u64);
+} counter SEC(".maps");
+
+SEC("kretprobe/handle_io")
+int bpf_prog1(struct pt_regs *ctx)
+{
+    struct kvm_vcpu *vcpu;
+    struct kvm_run *run;
+    __u64 *val;
+    __u32 key = 0;
+    unsigned long exit_qualification;
+    unsigned short port;
+
+    // Get reterun value
+    BPF_CORE_READ_INTO(&vcpu, ctx, di); 
+    bpf_core_read(&exit_qualification, sizeof(exit_qualification), &vcpu->arch.exit_qualification);
+    port = exit_qualification >> 16;
+
+    // Check if the port is 0 and increment the counter
+    if (port == TARGET_PORT) {
+        val = bpf_map_lookup_elem(&counter, &key);
+        if (val) {
+            (*val)++;
+        } else {
+            __u64 initial_value = 1;
+            bpf_map_update_elem(&counter, &key, &initial_value, BPF_ANY);
+        }
+    }
+
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
---
+++ ebpf_flab/kretprobe_kvm/port_trace.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include <unistd.h>
+
+#define COUNTER_MAP_NAME "counter"
+
+int main(int argc, char **argv)
+{
+    struct bpf_object *obj;
+    struct bpf_link *link;
+    int err;
+    int map_fd;
+
+    // BPFオブジェクトファイルを読み込む
+    obj = bpf_object__open_file("port_trace.bpf.o", NULL);
+    if (libbpf_get_error(obj)) {
+        fprintf(stderr, "Failed to open BPF object file: %d\n", (int)libbpf_get_error(obj));
+        return 1;
+    }
+
+    // BPFプログラムをロードする
+    err = bpf_object__load(obj);
+    if (err) {
+        fprintf(stderr, "Failed to load BPF object: %d\n", err);
+        return 1;
+    }
+
+    // BPFプログラムを取得
+    struct bpf_program *prog = bpf_object__find_program_by_name(obj, "bpf_prog1");
+    if (!prog) {
+        fprintf(stderr, "Failed to find BPF program: %s\n", strerror(errno));
+        return 1;
+    }
+
+    // kretprobeにBPFプログラムをアタッチ
+    link = bpf_program__attach(prog);
+    if (libbpf_get_error(link)) {
+        fprintf(stderr, "Failed to attach BPF program: %s\n", strerror(errno));
+        return 1;
+    }
+
+    // カウンターマップのファイルディスクリプタを取得
+    map_fd = bpf_object__find_map_fd_by_name(obj, COUNTER_MAP_NAME);
+    if (map_fd < 0) {
+        fprintf(stderr, "Failed to find BPF map: %s\n", strerror(errno));
+        return 1;
+    }
+
+    printf("BPF program loaded, attached successfully, and counter map found.\n");
+
+    // カウンター値を取得して表示するループ
+    __u32 key = 0;
+    __u64 value;
+    __u64 prev_value = 0;
+    while (1) {
+        sleep(5); // 5秒ごとにカウンター値を取得
+
+        if (bpf_map_lookup_elem(map_fd, &key, &value) == 0) {
+            printf("Counter value: %llu\n", value-prev_value);
+            prev_value = value;
+
+        } else {
+            fprintf(stderr, "Failed to read counter value: %s\n", strerror(errno));
+        }
+    }
+
+    // 後始末
+    bpf_link__destroy(link);
+    bpf_object__close(obj);
+
+    return 0;
+}
